b7e0e6579f2a8067bfd36d8b7d421ccc17041c5f
iketonic <ijeomahorjia@gmail.com>
2026-09-01 21:16:09 +0100

    github pages support

--- next.config.ts
+++ next.config.ts
@@ -1,52 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  headers: async () => {
-    return [
-      {
-        source: '/(.*?)',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-          {
-            key: 'X-Xss-Protection',
-            value: '1; mode=block',
-          },
-        ],
-      },
-      {
-        source: '/sw.js',
-        headers: [
-          {
-            key: 'Content-Type',
-            value: 'application/javascript; charset=utf-8',
-          },
-          {
-            key: 'Cache-Control',
-            value: 'no-cache, no-store, must-revalidate',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self'",
-          },
-        ],
-      },
-    ]
-  },
+  output: "export",
   poweredByHeader: false,
   reactStrictMode: true,
   allowedDevOrigins: [
-    '192.168.1.189'
+    'openhack.dev',
+    'www.openhack.dev',
   ],
   turbopack: {
     rules: {
