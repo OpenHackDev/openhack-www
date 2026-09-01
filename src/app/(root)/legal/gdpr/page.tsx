65a9c27b065aa9e3678cbb45ef6625b24b0bb022
iketonic <ijeomahorjia@gmail.com>
2026-09-01 19:34:42 +0100

    fixed buttons

--- src/app/(root)/legal/gdpr/page.tsx
+++ src/app/(root)/legal/gdpr/page.tsx
@@ -6,10 +6,12 @@ export default function() {
         <div className="p-2 pl-4 pr-4">
             <div>
                 <h1 className="text-4xl"><b>GDPR</b></h1>
-                <div className="text-gray-400"><i>
-                    <h2>Last revised: 01/09/2026</h2>
-                    <h2>Revised by: Ikechukwu</h2>
-                </i></div>
+                <div className="text-gray-400">
+                    <i>
+                        <h2>Last revised: 01/09/2026</h2>
+                        <h2>Revised by: Ikechukwu</h2>
+                    </i>
+                </div>
             </div>
             <hr className="bg-oh-blue w-full h-1 mx-auto my-4 bg-neutral-quaternary border-0 rounded-sm md:my-5"/>
             <MarkdownDocument content={GDPR} />
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -14,13 +14,39 @@ export default function() {
                     <hr className="bg-oh-blue w-100 h-1 mx-auto my-4 bg-neutral-quaternary border-0 rounded-sm md:my-5"/>
                     <div className="pt-2">
                         <div className="flex flex-1 flex-row gap-2">
-                            <Button className="bg-oh-blue border-oh-blue text-white">
-                                <Link rel="stylesheet" href="mailto:hello@openhack.dev">
+                                <a rel="stylesheet" href="mailto:hello@openhack.dev" className="
+                                    bg-oh-blue
+                                    border-oh-blue
+                                    text-white
+                                    flex
+                                    flex-col
+                                    items-center
+                                    justify-center
+                                    h-7.5 w-max
+                                    p-2
+                                    border-4
+                                    shadow-[2px_2px_0px_0px_rgba(0,0,0,1)]
+                                    active:translate-y-0.5
+                                    active:shadow-[2px_1px_0px_0px_rgba(0,0,0,1)]"
+                                >
                                     Register interest
-                                </Link>
-                            </Button>
+                                </a>
                             <span className="text-oh-blue mt-1">•</span>
-                            <Link rel="stylesheet" href="https://app.openhack.dev/auth/login"><Button>Login</Button></Link>
+                            <a rel="stylesheet" href="https://app.openhack.dev/auth/login" className="
+                                flex
+                                flex-col
+                                items-center
+                                justify-center
+                                h-7.5 w-max
+                                p-2
+                                border-4
+                                border-black
+                                shadow-[2px_2px_0px_0px_rgba(0,0,0,1)]
+                                active:translate-y-0.5
+                                active:shadow-[2px_1px_0px_0px_rgba(0,0,0,1)]"
+                            >
+                                Login
+                            </a>
                         </div>
                     </div>
                 </BorderedContainer>
